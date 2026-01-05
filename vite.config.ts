56c8f8d50a6ace809437ab63e91f59a0f66eb1c0
Alex Spurling <alexspurling@gmail.com>
2026-01-05 16:40:06 +0000

    Make server ports explicit

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: 5000,
+		strictPort: false,
+	},
+	preview: {
+		port: 4000,
+		strictPort: false,
+	}
 });
